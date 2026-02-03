c7dfb9d5d0424088033a944da56d6f880e9a7c27
ireck22 <dragon9029@gmail.com>
2026-02-03 16:27:01 +0800

    在class裡加上show(顯示購物車def)和最後顯示

--- main.py
+++ main.py
@@ -13,10 +13,17 @@ class bookcar:
             if self.total[e]['name']==name:
                 self.total[e]["qty"]+=qty
                 self.total[e]["sum"]+=sum
-            return 1
+                return 1
         self.total.append({"name":name,"price":price,"qty":qty,"sum":sum})
 
-
+    def show(self):
+        finish=""
+        for d in range(0,len(self.total)):
+           finish+=f"{self.total[d]['name']}-價格:{self.total[d]['price']}-數量:{self.total[d]['qty']}-小記:{self.total[d]['sum']}\n" 
+           self.totalsum+=self.total[d]["sum"] #計算總金額
+        finish+=f"==============共{self.totalsum}元===================="
+        return finish
+    
 book1=bookcar()
 item1=input("請輸入商品名稱和價格和數量?")
 item2=input("請輸入第二個商品名稱和價格和數量?")
@@ -25,3 +32,6 @@ item2_1=item2.split(',')
 
 book1.add(item1_1[0],int(item1_1[1]),int(item1_1[2]))
 book1.add(item2_1[0],int(item2_1[1]),int(item2_1[2]))
+
+result=book1.show()
+print(result)
