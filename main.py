022400422dc73e09e6ccbf9667cc1ece2dd561b8
ireck22 <dragon9029@gmail.com>
2026-02-03 16:37:11 +0800

    show 加上總金額初始化，class加上移除商品

--- main.py
+++ main.py
@@ -16,14 +16,26 @@ class bookcar:
                 return 1
         self.total.append({"name":name,"price":price,"qty":qty,"sum":sum})
 
-    def show(self):
+    def show(self): #顯示購物車
         finish=""
+        self.totalsum=0 #總金額初始化
         for d in range(0,len(self.total)):
            finish+=f"{self.total[d]['name']}-價格:{self.total[d]['price']}-數量:{self.total[d]['qty']}-小記:{self.total[d]['sum']}\n" 
            self.totalsum+=self.total[d]["sum"] #計算總金額
         finish+=f"==============共{self.totalsum}元===================="
         return finish
     
+    def rv_item(self,name): #以下是移除商品
+        try:
+            new_total=[]
+            for k in self.total:
+                if name in k['name']:
+                    continue
+                new_total.append(k)
+            self.total=new_total
+            return 1
+        except ZeroDivisionError as err:
+            return 2
 book1=bookcar()
 item1=input("請輸入商品名稱和價格和數量?")
 item2=input("請輸入第二個商品名稱和價格和數量?")
@@ -35,3 +47,7 @@ book1.add(item2_1[0],int(item2_1[1]),int(item2_1[2]))
 
 result=book1.show()
 print(result)
+
+book1.rv_item('java')
+result2=book1.show()
+print(result2)
